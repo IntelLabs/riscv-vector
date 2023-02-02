4a7d9fef87773037457ac19c92886e7f2d96b64c
Long Jiang <long.jiang@intel.com>
2023-02-02 12:18:04 +0800

    ALU top code. Build files

---
+++ fu_lsu/src/main/scala/vfu/VFuBundles.scala
@@ -0,0 +1,13 @@
+package yunsuan.vector
+
+import chisel3._
+import chisel3.util._
+
+class VFuCtrl extends Bundle {
+  val vm = Bool()
+  val ma = Bool()
+  val ta = Bool()
+  val lmul = UInt(3.W)
+  val vl = UInt(8.W)
+  val uopIdx = UInt(6.W)
+}
