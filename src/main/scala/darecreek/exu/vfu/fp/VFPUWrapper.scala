7ee9fa43bb589890478a2881373172088ec88ae2
Kou, Zhentao <zhentao.kou@intel.com>
2023-09-15 19:44:53 +0800

    compare fflag

--- src/main/scala/darecreek/exu/vfu/fp/VFPUWrapper.scala
+++ src/main/scala/darecreek/exu/vfu/fp/VFPUWrapper.scala
@@ -502,12 +502,12 @@ class VFPUWrapper(implicit p: Parameters) extends VFuModule {
   when(io.out.valid && io.out.ready) {
     red_fflag := 0.U
   }.elsewhen(red_out_valid && red_out_ready) {
-    red_fflag := red_fflag | fpu(0).io.out.bits.fflags
+    red_fflag := red_fflag | fpu(0).io.out.bits.fflags | fpu(1).io.out.bits.fflags
   }
 
   io.out.bits.vd := Mux(output_en, output_data, Mux(io.out.bits.uop.ctrl.narrow_to_1, cmp_tail_vd, Mux(io.out.bits.uop.ctrl.narrow, narrow_tail_vd, normal_tail_vd)))
   io.in.ready := fpu(0).io.in.ready & fpu(1).io.in.ready & io.out.ready & !red_busy
-  io.out.bits.fflags := Mux(vstart_gte_vl, 0.U, Mux(output_en, red_fflag, fpu(0).io.out.bits.fflags | fpu(1).io.out.bits.fflags))
+  io.out.bits.fflags := Mux(vstart_gte_vl, 0.U, Mux(output_en | io.out.bits.uop.ctrl.narrow_to_1, red_fflag, fpu(0).io.out.bits.fflags | fpu(1).io.out.bits.fflags))
   io.out.valid := Mux(output_en, output_valid, Mux(io.out.bits.uop.ctrl.narrow_to_1, io.out.bits.uop.uopEnd & fpu(0).io.out.valid & !red_busy, fpu(0).io.out.valid & !red_busy))
   red_in_ready := fpu(0).io.in.ready
   red_out_valid := fpu(0).io.out.valid
