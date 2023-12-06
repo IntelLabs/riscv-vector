93f139f7809c349393b2d2e9d5f30629275aadf3
Long Jiang <long.jiang@intel.com>
2023-12-06 13:43:37 +0800

    Fix bug of vdiv

--- src/main/scala/darecreek/exu/vfu/div/VDiv.scala
+++ src/main/scala/darecreek/exu/vfu/div/VDiv.scala
@@ -32,7 +32,6 @@ class VDiv(implicit p: Parameters) extends VFuModule { //with RequireAsyncReset 
 
   /** VFuInput -> LaneFuInput */
   for (i <- 0 until 2) {
-    // divTops(i).io.in.valid := io.in.valid
     divTops(i).io.in.bits.uop.connectFromVUop(io.in.bits.uop, isDiv = true)
     divTops(i).io.in.bits.vs1 := UIntSplit(io.in.bits.vs1, 64)(i)
     divTops(i).io.in.bits.vs2 := UIntSplit(io.in.bits.vs2, 64)(i)
@@ -43,26 +42,54 @@ class VDiv(implicit p: Parameters) extends VFuModule { //with RequireAsyncReset 
     divTops(i).io.in.bits.tail := UIntSplit(tailReorg, 8)(i)
   }
   
-  /** LaneFUOutput -> VFpOutput */
-  val outVdReg = Reg(Vec(2, UInt(64.W)))
-  val outfflagsReg = Reg(Vec(2, UInt(5.W)))
-  val outUopReg = Reg(new VUop)
-  val outValidReg = RegInit(VecInit.fill(2)(false.B))
-  val allOutValidRegClear = !outValidReg(0) && !outValidReg(1)
-  
-  io.out.valid := outValidReg(0) && outValidReg(1)
-
+  /** Flush gen */
   val currentNeedFlush = RegInit(false.B)
+  val in_robIdx = io.in.bits.uop.sysUop.robIdx
+  val currentRobIdx = RegEnable(in_robIdx, io.in.fire)
+  when (io.in.fire) {
+    currentNeedFlush := in_robIdx.needFlush(io.redirect)
+  }.otherwise {
+    currentNeedFlush := currentNeedFlush || currentRobIdx.needFlush(io.redirect)
+  }
   
+  /** Valid out */
+  val outValidReg = RegInit(VecInit.fill(2)(false.B))
+  val outValidFlush = RegInit(false.B)
+  val performOutFlush = outValidReg(0) && outValidReg(1) && outValidFlush
   for (i <- 0 until 2) {
     divTops(i).io.out.ready := true.B
     when (divTops(i).io.out.valid) {
-      // outValidReg(i) := true.B
-      outValidReg(i) := !(currentNeedFlush || divTops(i).io.out.bits.uop.sysUop.robIdx.needFlush(io.redirect))
-    }.elsewhen (io.out.fire) {
+      outValidReg(i) := true.B
+    }.elsewhen (io.out.fire || performOutFlush) {
       outValidReg(i) := false.B
     }
+  }
+  when (divTops(0).io.out.valid) {
+    outValidFlush := currentNeedFlush || divTops(0).io.out.bits.uop.sysUop.robIdx.needFlush(io.redirect)
+  }.elsewhen (divTops(1).io.out.valid) {
+    outValidFlush := currentNeedFlush || divTops(1).io.out.bits.uop.sysUop.robIdx.needFlush(io.redirect)
+  }.elsewhen (outValidReg(0) && outValidReg(1)) {
+    outValidFlush := false.B
+  }
+  io.out.valid := outValidReg(0) && outValidReg(1) && !outValidFlush
 
+  /** io.in.ready */
+  val isBusy = RegInit(false.B)
+  when (io.in.fire) {
+    isBusy := true.B
+  }.elsewhen (io.out.fire || performOutFlush) {
+    isBusy := false.B
+  }
+  for (i <- 0 until 2) {
+    divTops(i).io.in.valid := io.in.valid && !isBusy
+  }
+  io.in.ready := divTops(0).io.in.ready && divTops(1).io.in.ready && !isBusy
+
+  /** vd,uop out */
+  val outVdReg = Reg(Vec(2, UInt(64.W)))
+  val outfflagsReg = Reg(Vec(2, UInt(5.W)))
+  val outUopReg = Reg(new VUop)
+  for (i <- 0 until 2) {
     when (divTops(i).io.out.valid) {
       outVdReg(i) := divTops(i).io.out.bits.vd
       outfflagsReg(i) := divTops(i).io.out.bits.fflags
@@ -77,28 +104,7 @@ class VDiv(implicit p: Parameters) extends VFuModule { //with RequireAsyncReset 
   }
   io.out.bits.uop := outUopReg
 
-  val isBusy = RegInit(false.B)
-  when (io.in.fire) {
-    isBusy := true.B
-  }.elsewhen (io.out.fire) {
-    isBusy := false.B
-  }
-  // val outIsReady = io.out.fire || allOutValidRegClear
-  for (i <- 0 until 2) {
-    divTops(i).io.in.valid := io.in.valid && !isBusy
-  }
-  io.in.ready := divTops(0).io.in.ready && divTops(1).io.in.ready && !isBusy
 
-  /**
-   * Redirect handling
-   */
-  val in_robIdx = io.in.bits.uop.sysUop.robIdx
-  val currentRobIdx = RegEnable(in_robIdx, io.in.fire)
-  when (io.in.fire) {
-    currentNeedFlush := in_robIdx.needFlush(io.redirect)
-  }.otherwise {
-    currentNeedFlush := currentNeedFlush || currentRobIdx.needFlush(io.redirect)
-  }
 }
 
 import xiangshan._
