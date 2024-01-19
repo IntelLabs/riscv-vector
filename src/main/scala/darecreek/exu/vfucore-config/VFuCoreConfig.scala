7dfe684782ddf1951b21000797d03b79b685ec8b
Long Jiang <long.jiang@intel.com>
2024-01-19 13:46:29 +0800

    Rewrite VFUCore-config

--- src/main/scala/darecreek/exu/vfucore-config/VFuCoreConfig.scala
+++ src/main/scala/darecreek/exu/vfucore-config/VFuCoreConfig.scala
@@ -3,10 +3,17 @@ package darecreek.exu.vfucoreconfig
 import darecreek.VExpdUOp
 
 /**
+  * Choose which VUop/Redirect as the final ones
+  */
+class VUop extends VUop_darecreek
+class Redirect extends Redirect_darecreek
+
+
+/**
  *  Dare Creek
  */
-class VUop extends VExpdUOp {
+class VUop_darecreek extends VExpdUOp {
   def uopIdx = expdIdx
   def uopEnd = expdEnd
 }
-class Redirect extends darecreek.Redirect
+class Redirect_darecreek extends darecreek.Redirect
