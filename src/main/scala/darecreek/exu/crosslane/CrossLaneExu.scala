17583b9298acc479b3fa7f2a3de568207a0cf695
Kou <107921598+Kou66666@users.noreply.github.com>
2024-01-20 16:30:55 +0800

    reduction pass (#178)

--- src/main/scala/darecreek/exu/crosslane/CrossLaneExu.scala
+++ src/main/scala/darecreek/exu/crosslane/CrossLaneExu.scala
@@ -3,6 +3,9 @@ package darecreek
 import chisel3._
 import chisel3.util._
 import darecreek.exu.crosslane.vmask._
+import darecreek.exu.crosslane.reduction._
+import chipsalliance.rocketchip.config._
+import darecreek.exu.vfucore.{VFuModule, VFuParamsKey, VFuParameters}
 
 class VCrossLaneExu extends Module {
 
@@ -15,27 +18,32 @@ class VCrossLaneExu extends Module {
     val out = Decoupled(new VCrossExuOut)
   })
 
-  val permutation = Module(new Permutation)
-  val vmask = Module(new VMaskDC)
-  // val reduction = Module(new Reduction)
+  implicit val p = Parameters.empty.alterPartial({
+    case VFuParamsKey => VFuParameters(VLEN = 256)
+  })
 
-  // reduction.io.in.bits := io.in.bits
-  // reduction.io.in.valid := io.in.valid && io.in.bits.uop.ctrl.redu
-  io.in.readys(2) := true.B
- 
+  val permutation = Module(new Permutation)
+  val vmask = Module(new VMaskDC()(p))
+  val reduction = Module(new Reduction()(p))
+
+  reduction.io.in.bits := io.in.bits
+  reduction.io.in.valid := io.in.valid && io.in.bits.uop.ctrl.redu
+  reduction.io.redirect := 0.U.asTypeOf(new Redirect)  // !!!! flush
+  io.in.readys(2) := reduction.io.in.ready
+
   vmask.io.in.bits := io.in.bits
   vmask.io.in.valid := io.in.valid && io.in.bits.uop.ctrl.mask
   io.in.readys(0) := vmask.io.in.ready
- 
+
   permutation.io.in.bits := io.in.bits
   permutation.io.in.valid := io.in.valid && io.in.bits.uop.ctrl.perm
   io.in.readys(1) := permutation.io.in.ready
 
-  val arb = Module(new Arbiter(new VCrossExuOut, 2))
-  // arb.io.in(0) <> reduction.io.out
-  arb.io.in(0) <> vmask.io.out
-  arb.io.in(1) <> permutation.io.out
-  io.out <> arb.io.out  
+  val arb = Module(new Arbiter(new VCrossExuOut, 3))
+  arb.io.in(0) <> reduction.io.out
+  arb.io.in(1) <> vmask.io.out
+  arb.io.in(2) <> permutation.io.out
+  io.out <> arb.io.out
 }
 
 object VerilogCross extends App {
