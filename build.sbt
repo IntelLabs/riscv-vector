f75b795f6130031fcf3fe78e12fb50b682d5a8b7
CacheL1ne <32099670+miaochenlu@users.noreply.github.com>
2024-05-31 15:08:33 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,24 @@
+// See README.md for license details.
+
+ThisBuild / scalaVersion     := "2.13.12"
+ThisBuild / version          := "0.1.0"
+ThisBuild / organization     := "%ORGANIZATION%"
+
+val chiselVersion = "6.2.0"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "%NAME%",
+    libraryDependencies ++= Seq(
+      "org.chipsalliance" %% "chisel" % chiselVersion,
+      "org.scalatest" %% "scalatest" % "3.2.16" % "test",
+    ),
+    scalacOptions ++= Seq(
+      "-language:reflectiveCalls",
+      "-deprecation",
+      "-feature",
+      "-Xcheckinit",
+      "-Ymacro-annotations",
+    ),
+    addCompilerPlugin("org.chipsalliance" % "chisel-plugin" % chiselVersion cross CrossVersion.full),
+  )
---
+++ src/main/scala/gcd/GCD.scala
@@ -0,0 +1,46 @@
+// See README.md for license details.
+
+package gcd
+
+import chisel3._
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+import _root_.circt.stage.ChiselStage
+
+/**
+  * Compute GCD using subtraction method.
+  * Subtracts the smaller from the larger until register y is zero.
+  * value in register x is then the GCD
+  */
+class GCD extends Module {
+  val io = IO(new Bundle {
+    val value1        = Input(UInt(16.W))
+    val value2        = Input(UInt(16.W))
+    val loadingValues = Input(Bool())
+    val outputGCD     = Output(UInt(16.W))
+    val outputValid   = Output(Bool())
+  })
+
+  val x  = Reg(UInt())
+  val y  = Reg(UInt())
+
+  when(x > y) { x := x - y }
+    .otherwise { y := y - x }
+
+  when(io.loadingValues) {
+    x := io.value1
+    y := io.value2
+  }
+
+  io.outputGCD := x
+  io.outputValid := y === 0.U
+}
+
+/**
+ * Generate Verilog sources and save it in file GCD.v
+ */
+object GCD extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new GCD,
+    firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
+  )
+}
